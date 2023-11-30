6724f86e9da17a2b9f89505dc914d8f1323fbfcf
KevBou96 <kevenebou96@gmail.com>
2023-11-29 20:43:58 -0600

    creating angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'gym-match';
+  text: string = ''
+
+
+  pressedButton() {
+    this.text = 'nice'
+    console.log(this.text);
+  
+  }
 }
